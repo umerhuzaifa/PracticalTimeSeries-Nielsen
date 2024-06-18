cb69af5745ef94a6ab887365cae475b8aab2d40b
umerhuzaifa <umerhuzaifa@gmail.com>
2024-06-18 17:43:48 -0500

    cleaned and pruned the codes

--- Ch02/smoothing.R
+++
@@ -1,8 +0,0 @@
-require(data.table)
-
-air = fread('data/AirPassengers.csv')
-names(air) = c("Date", "Num")
-air[, Date:= as.Date(paste0(Date, "-01"), format = "%Y-%m-%d")]
-
-## loess smoothing
-loess.smooth(air$Date, air$Num)
